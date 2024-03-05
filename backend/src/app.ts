ddaf8e240035b1f10fb833a6ea2e336ff354db50
Evelina Kim <like17seventeen@gmail.com>
2024-03-05 07:41:54 +0300

    feat: создать конфиги

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -13,6 +13,13 @@ const { PORT = 3000 } = process.env;
 const app = express();
 mongoose.connect(DB_ADDRESS);
 
+// Краш-тест сервера
+app.get('/crash-test', () => {
+  setTimeout(() => {
+    throw new Error('Сервер сейчас упадёт');
+  }, 0);
+});
+
 // Только для локальных тестов. Не используйте это в продакшене
 // app.use(cors())
 app.use(express.json());
